881f0fd9dd9638c24c98a5b9ca3ca8562d8cbe33
Lester <lgdelacruz92@gmail.com>
2024-07-24 06:59:05 -0400

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,15 @@ export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    >
+      <Image
+        onMouseMove={handleMouseMove}
+        height={100}
+        width={100}
+        src={item.image}
+        alt={item.name}
+        className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
+      />
+    </main>
   );
 }
