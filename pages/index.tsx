1a3eab707e01beb98aff4eb6932dd61148e38cba
Lester <lgdelacruz92@gmail.com>
2024-07-24 07:11:07 -0400

    Added self image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useMotionValue } from "framer-motion";
+import SelfImage from "@/public/images/self.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const x = useMotionValue(0);
+
+  const handleMouseMove = (event: any) => {
+    const halfWidth = event.target.offsetWidth / 2;
+    x.set(event.nativeEvent.offsetX - halfWidth);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -12,8 +21,8 @@ export default function Home() {
         onMouseMove={handleMouseMove}
         height={100}
         width={100}
-        src={item.image}
-        alt={item.name}
+        src={SelfImage.src}
+        alt="Self"
         className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
       />
     </main>
