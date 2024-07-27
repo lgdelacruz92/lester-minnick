3672cdb359e65d5b05e35cf102e35c5f8a910d13
Lester <lgdelacruz92@gmail.com>
2024-07-27 06:28:37 -0400

    image fixed

--- pages/home/profile-photo.tsx
+++ pages/home/profile-photo.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
-import {
-  useMotionValue,
-  AnimatePresence,
-  motion,
-  useSpring,
-  useTransform,
-} from "framer-motion";
+
 import SelfImage from "@/public/images/self.jpg";
-import { useState } from "react";
 
 const SelfData = {
   name: "Lester Minnick",
@@ -15,65 +8,15 @@ const SelfData = {
 };
 
 export default function ProfilePhoto() {
-  const [showLabel, setShowLabel] = useState<boolean>(false);
-  const x = useMotionValue(0);
-
-  const handleMouseMove = (event: any) => {
-    const halfWidth = event.target.offsetWidth / 2;
-    x.set(event.nativeEvent.offsetX - halfWidth); // set the x value, which is then used in transform and rotate
-  };
-
-  const springConfig = { stiffness: 100, damping: 5 };
-  // translate the tooltip
-  const translateX = useSpring(
-    useTransform(x, [-100, 100], [-50, 50]),
-    springConfig,
-  );
-
-  const rotate = useSpring(
-    useTransform(x, [-100, 100], [-45, 45]),
-    springConfig,
-  );
-
   return (
-    <div
-      className="group relative"
-      onMouseEnter={() => setShowLabel(true)}
-      onMouseLeave={() => setShowLabel(false)}
-    >
-      <AnimatePresence mode="popLayout">
-        {showLabel && (
-          <motion.div
-            initial={{ opacity: 0, y: 20, scale: 0.6 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              scale: 1,
-              transition: {
-                type: "spring",
-                stiffness: 260,
-                damping: 10,
-              },
-            }}
-            exit={{ opacity: 0, y: 20, scale: 0.6 }}
-            style={{
-              translateX: translateX,
-              rotate: rotate,
-              whiteSpace: "nowrap",
-            }}
-            className="absolute -top-16 z-50 flex flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
-          >
-            <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
-            <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-            <div className="relative z-30 text-base font-bold text-white">
-              {SelfData.name}
-            </div>
-            <div className="text-xs text-white">{SelfData.title}</div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+    <div className="flex flex-col items-center">
+      <div className="flex items-center text-base font-bold text-black dark:text-white">
+        <span className="w-full text-center">{SelfData.name}</span>
+      </div>
+      <div className="mb-4 flex text-xs text-black dark:text-white">
+        <span className="w-full text-center">{SelfData.title}</span>
+      </div>
       <Image
-        onMouseMove={handleMouseMove}
         height={384}
         width={384}
         src={SelfImage.src}
