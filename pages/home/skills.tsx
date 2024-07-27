381ef8235a18056dcdf9eb601eed04df490d60cb
Lester <lgdelacruz92@gmail.com>
2024-07-27 06:48:50 -0400

    skills

--- pages/home/skills.tsx
+++ pages/home/skills.tsx
@@ -2,37 +2,38 @@
 import React, { useRef } from "react";
 import { CChart } from "@coreui/react-chartjs";
 import { ChartOptions } from "chart.js";
-import { getStyle } from '@coreui/utils';
+import { getStyle } from "@coreui/utils";
+import { GlareCard } from "@/components/ui/glare-card";
 
 const data = {
   labels: [
-    'Eating',
-    'Drinking',
-    'Sleeping',
-    'Designing',
-    'Coding',
-    'Cycling',
-    'Running',
+    "Eating",
+    "Drinking",
+    "Sleeping",
+    "Designing",
+    "Coding",
+    "Cycling",
+    "Running",
   ],
   datasets: [
     {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(220, 220, 220, 0.2)',
-      borderColor: 'rgba(220, 220, 220, 1)',
-      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-      pointBorderColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+      label: "My First dataset",
+      backgroundColor: "rgba(220, 220, 220, 0.2)",
+      borderColor: "rgba(220, 220, 220, 1)",
+      pointBackgroundColor: "rgba(220, 220, 220, 1)",
+      pointBorderColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220, 220, 220, 1)",
       data: [65, 59, 90, 81, 56, 55, 40],
     },
     {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(151, 187, 205, 0.2)',
-      borderColor: 'rgba(151, 187, 205, 1)',
-      pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-      pointBorderColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(151, 187, 205, 1)',
+      label: "My Second dataset",
+      backgroundColor: "rgba(151, 187, 205, 0.2)",
+      borderColor: "rgba(151, 187, 205, 1)",
+      pointBackgroundColor: "rgba(151, 187, 205, 1)",
+      pointBorderColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(151, 187, 205, 1)",
       data: [28, 48, 40, 19, 96, 27, 100],
     },
   ],
@@ -42,30 +43,31 @@ const options: ChartOptions = {
   plugins: {
     legend: {
       labels: {
-        color: getStyle('--cui-body-color'),
-      }
-    }
+        color: getStyle("--cui-body-color"),
+      },
+    },
   },
   scales: {
     r: {
       grid: {
-        color: getStyle('--cui-border-color-translucent'),
+        color: getStyle("--cui-border-color-translucent"),
       },
       ticks: {
-        color: getStyle('--cui-body-color'),
+        color: getStyle("--cui-body-color"),
       },
     },
   },
-}
-
-
+};
 
 export default function Skills() {
   const ref = useRef();
 
   return (
-    <div className="relative m-12">
-      <CChart type="radar" ref={ref} data={data} />
-    </div>
+    <GlareCard className="relative p-4">
+      <div className="flex items-center text-base font-bold text-white">
+        <span className="w-full text-center">Skills</span>
+      </div>
+      <CChart type="radar" ref={ref} data={data} width={150} height={75} />
+    </GlareCard>
   );
 }
