f58bf4f1c042184f68d66df27e7f5aead836e467
Lester <lgdelacruz92@gmail.com>
2024-07-27 15:30:06 -0400

    okay for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import ProfilePhoto from "./home/profile-photo";
 import Skills from "./home/skills";
+import { Meteors } from "@/components/ui/meteors";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,13 +10,14 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-zinc-700`}
     >
-      <div className="flex flex-col items-center space-y-4">
+      <div className="relative flex flex-col items-center space-y-4">
         <div className="w-full rounded-md bg-zinc-900">
           <ProfilePhoto />
         </div>
         <div className="w-full rounded-md bg-zinc-900">
           <Skills />
         </div>
+        <Meteors number={10} />
       </div>
     </main>
   );
