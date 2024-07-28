f72496eaeeb85d63279900a07d446171845278aa
Lester <lgdelacruz92@gmail.com>
2024-07-28 06:58:34 -0400

    work experience

--- pages/home/work-experience/experience.tsx
+++ pages/home/work-experience/experience.tsx
@@ -4,7 +4,7 @@ import DerivcoImage from "@/public/images/derivco.jpeg";
 
 export default function Experience() {
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <div>
         <Image
           src={DerivcoImage.src}
@@ -13,6 +13,13 @@ export default function Experience() {
           alt="derivco image"
           className="rounded-md"
         />
+      </div>
+      <div className="ml-4 grow">
+        <div className="mb-2 text-center uppercase text-white">
+          Full Stack Software Engineer
+        </div>
+        <div className="text-left text-zinc-400">Derivco</div>
+        <div className="text-left text-zinc-400">June 2023 - Present</div>
       </div>
     </div>
   );
