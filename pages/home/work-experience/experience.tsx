11be4cf78fbd520b376a266b641ee3c664fc67c8
Lester <lgdelacruz92@gmail.com>
2024-07-29 06:59:32 -0400

    experience details component

--- pages/home/work-experience/experience.tsx
+++ pages/home/work-experience/experience.tsx
@@ -2,25 +2,26 @@
 import Image from "next/image";
 import DerivcoImage from "@/public/images/derivco.jpeg";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useId, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CloseIcon from "@/components/common/close-icon";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 
 type ExperienceDetailsData = {
   title: string;
   id: number;
-  description: string;
-  ctaText: string;
-  ctaLink: string;
+  description: string[];
   src: string;
 };
 
 const sampleExperienceDetailsData: ExperienceDetailsData = {
   title: "Full Stack Software Engineer",
   id: 123,
-  description: "description",
-  ctaText: "ctaText",
-  ctaLink: "ctaLink",
+  description: [
+    "Python, and ML tools",
+    "Contributing to banking system app VueJS, HTML, CSS, Typescript, Docker, Kubernetes, Azure, Cypress, and Playwright",
+    "Created a file converter to convert our tests written in Cypress into Playwright tests saving devs hundreds of hours of manual conversion",
+    "Serviced .NET application for external customers",
+  ],
   src: "src",
 };
 
@@ -74,7 +75,7 @@ export default function Experience() {
             <motion.div
               layoutId={`card-${active.title}`}
               ref={ref}
-              className="relative flex h-full w-full max-w-[500px] flex-col overflow-hidden bg-zinc-700 sm:rounded-3xl md:h-fit md:max-h-[90%]"
+              className="relative flex h-full w-full max-w-[500px] flex-col overflow-hidden rounded bg-zinc-700 shadow-2xl"
               initial={{
                 opacity: 0,
                 width: 0,
@@ -129,30 +130,20 @@ export default function Experience() {
               </motion.div>
 
               <div>
-                <div className="flex items-start justify-between p-4">
-                  <div className="">
-                    <motion.h3
-                      layoutId={`title-${active.title}`}
-                      className="font-bold text-neutral-700 dark:text-neutral-200"
-                    >
-                      {active.title}
-                    </motion.h3>
-                    <motion.p
-                      layoutId={`description-${active.description}`}
-                      className="text-neutral-600 dark:text-neutral-400"
-                    >
-                      {active.description}
-                    </motion.p>
-                  </div>
-
-                  <motion.a
-                    layoutId={`button-${active.title}`}
-                    href={active.ctaLink}
-                    target="_blank"
-                    className="rounded-full bg-green-500 px-4 py-3 text-sm font-bold text-white"
+                <div className="p-4">
+                  <motion.h3
+                    layoutId={`title-${active.title}`}
+                    className="font-bold text-zinc-100"
                   >
-                    {active.ctaText}
-                  </motion.a>
+                    {active.title}
+                  </motion.h3>
+                  <div className="max-h-[250px] overflow-y-scroll text-neutral-600 text-zinc-300">
+                    <ul className="list-disc">
+                      {active.description.map((d, i) => {
+                        return <li key={`description-${i}`}>{d}</li>;
+                      })}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </motion.div>
