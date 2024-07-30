4612b15d5ab10e3d0fa53c1ece761e6b57a8fe0d
Lester <lgdelacruz92@gmail.com>
2024-07-30 06:07:11 -0400

    sweet experience is ready

--- pages/home/work-experience/experience.tsx
+++ pages/home/work-experience/experience.tsx
@@ -6,11 +6,50 @@ import CloseIcon from "@/components/common/close-icon";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 import { ExperienceDetailsData } from "./types";
 
-export default function Experience({
-  experienceData,
-}: {
-  experienceData: ExperienceDetailsData;
-}) {
+const experienceData: ExperienceDetailsData = {
+  title: "Full Stack Software Engineer",
+  id: 123,
+  description: [
+    "Python, and ML tools",
+    "Contributing to banking system app VueJS, HTML, CSS, Typescript, Docker, Kubernetes, Azure, Cypress, and Playwright",
+    "Created a file converter to convert our tests written in Cypress into Playwright tests saving devs hundreds of hours of manual conversion",
+    "Serviced .NET application for external customers",
+  ],
+  companyName: "Derivco",
+  duration: "June 2023 - Present",
+  src: "/images/derivco.jpeg",
+};
+
+const experienceData2: ExperienceDetailsData = {
+  title: "React Software Engineer",
+  id: 124,
+  description: [
+    "Python and ML Tools for data analysis",
+    "Developed and maintained features for the Leap App using React, OpenAI, NextJS, HTML, CSS, Docker, Kubernetes, SQL (Postgress), Google Cloud Console, Firestore, Fast API, Python, and BigQuery.",
+    "Created a slideshow experience showcasing potential rental spaces for retail brands in React, resulting in a 95% customer satisfaction rate.",
+    "Serviced .NET application from external customers.",
+  ],
+  companyName: "Leap",
+  duration: "May 2022 - April 2023",
+  src: "/images/leap.jpeg",
+};
+
+const experienceData3: ExperienceDetailsData = {
+  title: "Full Stack Software Engineer",
+  id: 125,
+  description: [
+    "Maintained and developed user content authoring platform using EmberJS, AWS, Python, Flask, Docker, Kubernetes, and TDD.",
+    "Worked with .NET Core Web API and Azure ecosystem (hosting, api, storage, and cloud functions)",
+    "Developed an XML parsing system to convert existing online textbooks into ZyBooks, reducing creation time significantly.",
+  ],
+  companyName: "Zybooks (Wiley)",
+  duration: "Jan 2020 - May 2022",
+  src: "/images/zybooks.jpeg",
+};
+
+const data = [experienceData, experienceData2, experienceData3];
+
+export default function Experience() {
   const ref = useRef<HTMLDivElement>(null);
   const [active, setActive] = useState<null | ExperienceDetailsData>(null);
 
@@ -33,34 +72,37 @@ export default function Experience({
 
   useOutsideClick(ref, () => setActive(null));
   return (
-    <div
-      className="flex w-full p-4 hover:bg-zinc-700"
-      onClick={() => setActive(experienceData)}
-    >
-      <Image
-        src={experienceData.src}
-        width={100}
-        height={100}
-        alt="derivco image"
-        className="rounded"
-      />
-      <div className="ml-4 grow">
-        <div className="mb-2 text-left uppercase text-white">
-          {experienceData.title}
-        </div>
-        <div className="text-left text-zinc-400">
-          {experienceData.companyName}
-        </div>
-        <div className="text-left text-zinc-400">{experienceData.duration}</div>
-        <button
-          className="rounded-full bg-cyan-600 px-4 text-white"
+    <>
+      {data.map((d) => (
+        <div
+          key={d.id}
+          className="flex w-full p-4 hover:bg-zinc-700"
           onClick={() => {
-            setActive(experienceData);
+            setActive(d);
           }}
         >
-          Details
-        </button>
-      </div>
+          <Image
+            src={d.src}
+            width={100}
+            height={100}
+            alt="derivco image"
+            className="rounded"
+          />
+          <div className="ml-4 grow">
+            <div className="mb-2 text-left uppercase text-white">{d.title}</div>
+            <div className="text-left text-zinc-400">{d.companyName}</div>
+            <div className="text-left text-zinc-400">{d.duration}</div>
+            <button
+              className="rounded-full bg-cyan-600 px-4 text-white"
+              onClick={() => {
+                setActive(d);
+              }}
+            >
+              Details
+            </button>
+          </div>
+        </div>
+      ))}
       <AnimatePresence>
         {active && typeof active === "object" ? (
           <div className="fixed inset-0 z-[100] grid place-items-center">
@@ -118,7 +160,7 @@ export default function Experience({
                     priority
                     width={200}
                     height={200}
-                    src={experienceData.src}
+                    src={active.src}
                     alt={active.title}
                     className="h-32 w-32 -translate-x-[4px] object-cover object-top"
                   />
@@ -142,6 +184,6 @@ export default function Experience({
           </div>
         ) : null}
       </AnimatePresence>
-    </div>
+    </>
   );
 }
