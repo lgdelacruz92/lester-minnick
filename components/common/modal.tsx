c80bf54713b0f6d3ed453198d3e5c8e88c4a4014
Lester <lgdelacruz92@gmail.com>
2024-08-03 09:09:15 -0400

    modal is created

--- components/common/modal.tsx
+++ components/common/modal.tsx
@@ -2,6 +2,8 @@
 import { Dispatch, SetStateAction, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
+import MobileIcon from "../icons/mobile-icon";
+import EmailIcon from "../icons/email-icon";
 
 const backdropVariants = {
   visible: { opacity: 1 },
@@ -29,8 +31,20 @@ export default function Modal({ showModal, setShowModal }: ModalProps) {
           id="modal-item-123"
           className="fixed left-0 top-0 h-full w-full bg-black/40"
         >
-          <div className="absolute left-[50%] top-[50%] translate-x-[-50%] bg-white">
-            Hello world
+          <div className="absolute left-[50%] top-[50%] flex translate-x-[-50%] flex-col rounded-lg bg-white p-4">
+            <div className="text-center font-bold opacity-70">Contacts</div>
+            <div className="flex flex-row items-center opacity-50">
+              <span>
+                <EmailIcon width="16" height="16" fill="var(--cui-gray)" />
+              </span>
+              <span className="ml-2">email: lgdelacruz92@gmail.com</span>
+            </div>
+            <div className="flex flex-row items-center opacity-50">
+              <span>
+                <MobileIcon width="16" height="16" fill="var(--cui-gray)" />
+              </span>
+              <span className="ml-2">phone: 805-319-8724</span>
+            </div>
           </div>
         </motion.div>
       )}
