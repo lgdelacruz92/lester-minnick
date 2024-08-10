e6ec80a80ec30f6f127ee6e1ee3603b17cdb007f
Lester <lgdelacruz92@gmail.com>
2024-08-10 06:41:38 -0400

    Added all references

--- static-data/references-data.ts
+++ static-data/references-data.ts
@@ -17,4 +17,45 @@ export const referencesData: Array<ReferenceData> = [
     review:
       "Lester's a productive engineer, and a proactive, good-natured teammate. He was well-respected by his team and manager, and I hope to work with him again in the future!",
   },
+  {
+    id: "3",
+    name: "Burton Tripathi",
+    src: "/images/burton.jpeg",
+    title: "Previous CEO",
+    review:
+      "Lester was a pleasure to work with. He is smart, thoughtful, humble, and interesting. He fits in well and always brings a smile and positive attitude. You will not regret time spent with Lester.",
+  },
+  {
+    id: "4",
+    name: "Josh Isaac",
+    src: "/images/josh.jpeg",
+    title: "Former Teammate",
+    review:
+      "Lester is super sharp. In the brief period I worked with him, he taught me about the details of our front end XML code, how to get the arcane properties of Visual Studio to play nice when debugging and running tests, and how you might use lambdas in C++. He's super friendly, highly motivated, and always trying to learn something new. Can't recommend him enough.",
+  },
+
+  {
+    id: "5",
+    title: "Former Teammate",
+    name: "Chen Chen",
+    src: "/images/chen.jpeg",
+    review:
+      "I worked with Lester for a year at TrueVision Systems. He is really good at learning and applying the knowledge. Also, his teamwork impressed me deeply. Lester is a good learner and he enjoys learning. He can learn new skill very quickly and use them in the work. Though he already have the skills to resolve the problems descently, he still tries to explore new Cpp 11 or 17 features, or new design patterns in his programming to do the things more elegantly. He also helped engineers of another team set up our distributed Jenkins test server, though it was not his domain. After that he became the tech lead of testing and guided us a a lot on it. Moreover, Lester likes sharing knowledges after he learn it. He always sends the email to the team to introduce those new knowledges and documents them explicitly on Confluence, our knowledge sharing web. When I ask questions in his domain, he always gives me detailed answer and sometimes pair-programs with me. He is a real team worker. Also he is really a funny guy and awesome photographer! It’s very exciting and it’s my pleasure to work with him.",
+  },
+  {
+    id: "6",
+    title: "Former Teammate",
+    name: "Logan Scheiner",
+    src: "/images/josh.jpeg",
+    review:
+      "I met Lester at TrueVision Systems, Inc. and worked alongside him developing software for approximately one year. Lester was a great asset to our team: his exploratory nature is well suited to unraveling complex problems. Whether it's integrating new technologies to existing systems or simply wading into uncharted code, Lester's able to dive in, learn about the environment, and make considerable progress. What I see in Lester is a true passion for learning new skills, technologies, and processes. It's a pleasure to work and collaborate with Lester. I always feel comfortable asking him for assistance or clarification. While pairing and during code review he takes his time to elaborate on questions and gives and receives constructive criticism well. His sincere and energetic personality is a welcome addition to any day of the week.",
+  },
+  {
+    id: "7",
+    title: "Former Teammate",
+    name: "Nicholas Tovar",
+    src: "/images/nick.jpeg",
+    review:
+      "Lester and I first worked together at TrueVision when he was an intern and then after he successfully made the jump to full-time employee. Even though I can't speak directly to his strengths as a programmer, I do know that he is a great person to have on a team and an asset to any company. He is dedicated to his work and takes his tasks very seriously. He also has a thirst for knowledge and an interest in understanding. This is demonstrated through some of his post here on LinkedIn and his general attitude in daily life. I remember every time I would run into him he would tell me about a new aspect of a project he was working on, but instead of complaining about having to learn something new, he reveled in it, even taking the time to help me understand concepts that he was just beginning to master. Lester's general positive attitude and helpful nature make him a pleasure to work with and when paired with his great work.",
+  },
 ];
